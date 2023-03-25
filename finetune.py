e54db5c0e40ee8129a97d87a098c75665a47638b
Terai Kenji <kenji@tahomatx.com>
2023-03-25 15:35:58 +0900

    modify

--- finetune.py
+++ finetune.py
@@ -86,7 +86,6 @@ def train(
     model = LlamaForCausalLM.from_pretrained(
         base_model,
         load_in_8bit=True,
-        torch_dtype=torch.float16,
         device_map=device_map,
     )
 
